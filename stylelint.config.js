b9233713a749f73fb0b820ff7cc22992355a3a38
levinhax <levin1229@163.com>
2021-01-11 19:11:29 +0800

    feat: 代码规范

---
+++ stylelint.config.js
@@ -0,0 +1,23 @@
+const config = {
+  root: true,
+  plugins: ['stylelint-order'],
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['function', 'if', 'each', 'include', 'mixin'],
+      },
+    ],
+    // 指定声明块内属性的字母顺序
+    'order/properties-order': [],
+  },
+}
+
+export default config

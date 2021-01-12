6a81a7015e1260c0eb8b75b1a7edcaf528987686
levinhax <levin1229@163.com>
2021-01-12 17:12:05 +0800

    feat: vite config配置

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { createApp } from 'vue'
 import App from './App'
 // import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-import './styles/index.scss'
+// import './styles/index.scss'
+import '/@/styles/index.scss'
 
 const app = createApp(App)
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,48 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueJsx from '@vitejs/plugin-vue-jsx' // build typecheck 依赖需要升级
+
+const pathResolve = (dir: string) => {
+  return path.resolve(__dirname, dir)
+}
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx()
-  ],
+  // 静态资源目录
+  // assetsInclude: 'public',
+  // 导入文件夹别名
+  alias: [{ find: '/@', replacement: pathResolve('./src') }],
+  // 配置Dep优化行为
   // optimizeDeps: {
-  //   exclude: ['ant-design-vue', 'lodash']
-  // }
+  // 这个命令专门为解决模块引用的坑而开发
+  // 例如我们要在 vite 中使用 lodash，只需要在 vite.config.js （vite 配置文件）中，配置 optimizeDeps 对象，在 include 数组中添加 lodash
+  //   include: ['lodash'],
+  // },
+  // optimizeDeps: {
+  //   include: ['axios', 'nprogress', 'mockjs']
+  // },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$injectedColor: orange;`
+      }
+    }
+  },
+  // 代理
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3001/',
+  //       changeOrigin: true,
+  //       rewrite: path => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  //   port: 8080, // 服务端口
+  // },
+  build: {
+    // sourcemap: true,
+    // cssCodeSplit: true
+  },
+  // 插件
+  plugins: [vue(), vueJsx()],
 })
